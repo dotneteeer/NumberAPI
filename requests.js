763fee5937c8fabfd7cc0e13b5e0a869d5434afc
Marat <447599489marat@gmail.com>
2024-05-04 11:54:22 +0300

    unnecessary removed

--- requests.js
+++ requests.js
@@ -2,7 +2,6 @@ export async function GetNumberDescription(number, callback){
     const response=await fetch("http://numbersapi.com/"+number)
     if(!response.ok)throw new Error("An error occured while processing the request: "+e)
     const responseText=await response.text()
-    console.log(responseText)
     callback(responseText)
     
     
