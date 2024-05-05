33639c5f746d6a7511410a9bf43f28567b2cf1b0
Marat <447599489marat@gmail.com>
2024-05-05 10:09:12 +0300

    Max random value fixed

--- JS/random_button.js
+++ JS/random_button.js
@@ -1,12 +1,12 @@
 random_number.addEventListener('click', function(){
-    const number=Math.floor(Math.random() * 1000)
+    const number=Math.floor(Math.random() * 1001)
     number_input.value=number;
     SubmitForm(number_form)
 })
 
 random_date.addEventListener('click', function(){
-    const day=Math.floor(Math.random() * 31)
-    const month=Math.floor(Math.random() * 12)
+    const day=Math.floor(Math.random() * 32)
+    const month=Math.floor(Math.random() * 13)
     day_input.value=day
     month_input.value=month
     SubmitForm(date_form)
